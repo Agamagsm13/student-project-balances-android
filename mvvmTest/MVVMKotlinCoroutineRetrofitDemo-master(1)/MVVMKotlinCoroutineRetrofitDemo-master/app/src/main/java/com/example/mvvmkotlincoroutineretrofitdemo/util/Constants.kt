ce0b3574468acf771e4d6e25eff94ee609003bff
Vyacheslav Borodkin <slavagsm13@mail.ru>
2020-02-25 13:31:04 +0300

    /

--- mvvmTest/MVVMKotlinCoroutineRetrofitDemo-master(1)/MVVMKotlinCoroutineRetrofitDemo-master/app/src/main/java/com/example/mvvmkotlincoroutineretrofitdemo/util/Constants.kt
+++ mvvmTest/MVVMKotlinCoroutineRetrofitDemo-master(1)/MVVMKotlinCoroutineRetrofitDemo-master/app/src/main/java/com/example/mvvmkotlincoroutineretrofitdemo/util/Constants.kt
@@ -1,6 +1,6 @@
 package com.example.mvvmkotlincoroutineretrofitdemo.util
 
 object Constants {
-    const val BASE_URL = "http://3.248.170.197:8888/bcv/quotes/bars/"
-    const val GET_USERS = "btc-usd/1415404800/1416268800"
+    const val BASE_URL = "http://3.248.170.197:8888/bcv/quotes/bars"
+    const val GET_USERS = "/btc-usd/1415404800/1416268800"
 }
